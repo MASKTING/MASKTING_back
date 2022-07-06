f20703a45dcb732f45ec79feb37fe82421758b2f
parkmuhyeun <pjhg410@gmail.com>
2022-07-06 21:38:21 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/java/com/maskting/backend/MasktingApplication.java
@@ -0,0 +1,13 @@
+package com.maskting.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasktingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasktingApplication.class, args);
+	}
+
+}

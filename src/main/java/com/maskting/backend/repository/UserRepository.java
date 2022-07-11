42207d66da64003e174999b6f52792f1c0e7c879
parkmuhyeun <pjhg410@gmail.com>
2022-07-11 10:10:37 +0900

    feat: oauth 서비스 추가

--- src/main/java/com/maskting/backend/repository/UserRepository.java
+++ src/main/java/com/maskting/backend/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.maskting.backend.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Long, User> {
+    User findByProviderId(String providerId);
 }

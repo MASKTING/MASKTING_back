e1c4f99ea56fe6db42ffae8279913bed9440c8db
parkmuhyeun <pjhg410@gmail.com>
2022-07-12 10:56:50 +0900

    feat: JwtUtil 생성

---
+++ src/main/java/com/maskting/backend/util/JwtUtil.java
@@ -0,0 +1,95 @@
+package com.maskting.backend.util;
+
+import com.maskting.backend.domain.oauth.UserPrincipal;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${app.auth.accessTokenExpiry}")
+    private long accessTokenValidTime;
+
+    @Value("${app.auth.refreshTokenExpiry}")
+    private long refreshTokenValidTime;
+
+    private static final String AUTHORITIES_KEY = "role";
+
+    private Key getSigningKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String createToken(String providerId, long time) {
+        Claims claims = Jwts.claims().setSubject(providerId);
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + time))
+                .signWith(getSigningKey(secretKey), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String createAccessToken(String providerId) {
+        return createToken(providerId, accessTokenValidTime);
+    }
+
+    public String createRefreshToken(String providerId) {
+        return createToken(providerId, refreshTokenValidTime);
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaimsJws(token).getBody();
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(new String[]{claims.get(AUTHORITIES_KEY).toString()})
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+        User user = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(user, "", authorities);
+    }
+
+    public String getProviderId(String token) {
+        try {
+            return getClaimsJws(token).getBody().getSubject();
+        } catch(ExpiredJwtException e) {
+            return e.getClaims().getSubject();
+        }
+    }
+
+    private Jws<Claims> getClaimsJws(String token) {
+        return Jwts.parserBuilder().setSigningKey(getSigningKey(secretKey)).build().parseClaimsJws(token);
+    }
+
+    public Boolean isTokenExpired(String token) {
+        Jws<Claims> claims = getClaimsJws(token);
+        return !claims.getBody().getExpiration().before(new Date());
+    }
+
+    public boolean validateToken(String token, UserPrincipal userPrincipal) {
+        try {
+            String providerId = getProviderId(token);
+            return providerId.equals(userPrincipal.getName())&& isTokenExpired(token);
+        } catch(Exception e) {
+            return false;
+        }
+    }
+}

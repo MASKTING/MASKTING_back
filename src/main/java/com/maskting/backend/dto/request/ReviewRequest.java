8a73e6741ce79dc9d94064ea70211d7ab98b6fa8
parkmuhyeun <pjhg410@gmail.com>
2022-08-12 11:29:28 +0900

    feat: returnGuests Dto 생성

---
+++ src/main/java/com/maskting/backend/dto/request/ReviewRequest.java
@@ -0,0 +1,16 @@
+package com.maskting.backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewRequest {
+    private int draw;
+    private int start;
+    private int length;
+}

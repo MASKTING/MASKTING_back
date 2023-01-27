c1ad537373d8a26f878a67a36349944865d13dd4
parkmuhyeun <pjhg410@gmail.com>
2023-01-27 23:57:33 +0900

    feat: 인증번호 도메인 생성

---
+++ src/main/java/com/maskting/backend/repository/VerificationNumberRepository.java
@@ -0,0 +1,6 @@
+package com.maskting.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface VerificationNumberRepository extends CrudRepository<com.maskting.backend.domain.VerificationNumber, String> {
+}
